5d235f4e68479a528694db0bf1a0a819b4db6d9c
Ziad-Elshemy <zizoelshemy@gmail.com>
2025-03-27 16:50:48 +0200

    map screen almost done

--- app/src/main/java/eg/iti/mad/climaguard/map/MapViewModel.kt
+++ app/src/main/java/eg/iti/mad/climaguard/map/MapViewModel.kt
@@ -1,5 +1,8 @@
 package eg.iti.mad.climaguard.map
 
+import android.content.Context
+import android.location.Geocoder
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -10,8 +13,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.util.Locale
 
 class MapViewModel(private val repo: Repository) : ViewModel() {
+
+    private val TAG = "MapViewModel"
+
     private val _searchQuery = MutableSharedFlow<String>()
     val searchQuery = _searchQuery.asSharedFlow()
 
@@ -23,6 +30,9 @@ class MapViewModel(private val repo: Repository) : ViewModel() {
 
     private val _selectedLocation = MutableStateFlow<LatLng?>(null)
     val selectedLocation: StateFlow<LatLng?> = _selectedLocation
+
+    private val mutableMessage = MutableSharedFlow<String>()
+    val message = mutableMessage.asSharedFlow()
 
     init {
         observeSearchQuery()
@@ -60,6 +70,59 @@ class MapViewModel(private val repo: Repository) : ViewModel() {
             emptyList()
         }
     }
+
+
+    fun addLocationToFav(location: LocationEntity){
+        viewModelScope.launch(Dispatchers.IO) {
+            if (location!=null){
+                try {
+                    val result = repo.addLocation(location)
+                    if (result>0){
+                        mutableMessage.emit("${location.name} Added Successfully!")
+                        Log.d(TAG, "addLocationToFav: success")
+                    }else{
+                        mutableMessage.emit("Product is already in Fav")
+                        Log.d(TAG, "addLocationToFav: already in Fav")
+                    }
+                }catch (ex:Exception){
+                    mutableMessage.emit("Couldn't added Location :${ex.message}")
+                    Log.d(TAG, "addLocationToFav: Couldn't added")
+                }
+
+            }else{
+                mutableMessage.emit("Couldn't added Location :Missing data")
+                Log.d(TAG, "addLocationToFav: :Missing data")
+            }
+        }
+    }
+
+    fun getCityAndCountryFromLocation(lat: Double, lon: Double, context: Context, onResult: (String, String) -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val geocoder = Geocoder(context, Locale.getDefault())
+                val addresses = geocoder.getFromLocation(lat, lon, 1)
+                if (!addresses.isNullOrEmpty()) {
+                    val city = addresses[0].locality ?: addresses[0].adminArea ?: "Unknown City"
+                    val country = addresses[0].countryName ?: "Unknown Country"
+                    Log.d(TAG, "getCityAndCountryFromLocation: ${addresses.get(0).toString()}")
+                    withContext(Dispatchers.Main) {
+                        onResult(city, country)
+                    }
+                } else {
+                    withContext(Dispatchers.Main) {
+                        onResult("Unknown City", "Unknown Country")
+                    }
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    onResult("Unknown City", "Unknown Country")
+                }
+            }
+        }
+    }
+
+
+
 }
 
 class MapFactory(private val repo: Repository) : ViewModelProvider.Factory {
